17631a867ad12db692c4497b02f3dd5485e8ce43
settheworldonfireiii <radke149@umn.edu>
2026-09-17 18:31:05 -0500

    ALLE FEHLER GROSSE DATEI ZESTORT

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='luffy',
+    version='0.0.0',
+    description='LUFFY: Learning to Reason under Off-Policy Guidance',
+    author='Jianhao Yan',
+    packages=find_packages(include=['deepscaler',]),
+    install_requires=[
+        'google-cloud-aiplatform',
+        'pylatexenc',
+        'sentence_transformers',
+        'tabulate',
+        'math-verify[antlr4_9_3]==0.6.0',
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.9',
+)
